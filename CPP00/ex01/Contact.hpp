bf122bfce1bc67206116aeaeab8c2592a700c376
Muhammad azfar Ahmad jefiruddin <mahmad-j@u91z01s06.42kl.edu.my>
2022-08-02 20:44:16 +0800

    attempt ex01 in CPP00

---
+++ CPP00/ex01/Contact.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mahmad-j <mahmad-j@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 19:36:11 by mahmad-j          #+#    #+#             */
+/*   Updated: 2022/08/02 20:40:32 by mahmad-j         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <iostream>
+# include <string>
+
+class	Contact {
+	private:
+		enum info {
+			name = 0
+		}
+}
+
+#endif
